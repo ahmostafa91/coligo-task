a2e2cd7b944433be47ee01fc7342848838a097a1
ahmed <ahmedmostafa05031991@gmail.com>
2019-04-13 12:23:38 +0200

    add build

---
+++ build/precache-manifest.d8b10c8db7e83c397f91fc6d44214968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac61208f9a46c167543b",
+    "url": "/static/css/main.667f0aa8.chunk.css"
+  },
+  {
+    "revision": "ac61208f9a46c167543b",
+    "url": "/static/js/main.0629809f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bf28b6b5532ae836ab6d",
+    "url": "/static/js/2.0833c125.chunk.js"
+  },
+  {
+    "revision": "228ae12e2db7833ff797d6afe9ca00fe",
+    "url": "/index.html"
+  }
+];
